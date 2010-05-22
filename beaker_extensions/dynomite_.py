ce097c988ada5365c8a9b355b0406cc0af62b808
Ben Bangert <ben@groovie.org>
2010-05-21 18:10:17 -0700

    Refactor for being a proper Python package.

---
+++ beaker_extensions/dynomite_.py
@@ -0,0 +1,53 @@
+import logging
+from beaker_extensions.nosql import Container
+from beaker_extensions.nosql import InvalidCacheBackendError
+from beaker_extensions.nosql import NoSqlManager
+ 
+try:
+    from dynomite import Dynomite
+    from dynomite.ttypes import *
+except ImportError:
+    raise InvalidCacheBackendError("Dynomite cache backend requires the 'dynomite' library")
+
+try: 
+    from thrift import Thrift
+    from thrift.transport import TSocket
+    from thrift.transport import TTransport
+    from thrift.protocol import TBinaryProtocol
+except ImportError:
+    raise InvalidCacheBackendError("Dynomite cache backend requires the 'thrift' library")
+ 
+log = logging.getLogger(__name__)
+ 
+class DynomiteManager(NoSqlManager):
+    def __init__(self, namespace, url=None, data_dir=None, lock_dir=None, **params):
+        NoSqlManager.__init__(self, namespace, url=url, data_dir=data_dir, lock_dir=lock_dir, **params)
+
+    def open_connection(self, host, port):
+        self.transport = TSocket.TSocket(host, int(port))
+        self.transport = TTransport.TBufferedTransport(transport)
+        self.protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        self.db_conn = Dynomite.Client(protocol)
+        self.transport.open()
+
+    def __contains__(self, key):
+        return self.db_conn.has(self._format_key(key))
+
+    def has_key(self, key):
+        return key in self
+
+    def set_value(self, key, value):
+        self.db_conn.put(self._format_key(key), None, value)
+
+    def __delitem__(self, key):
+        self.db_conn.remove(self._format_key(key))
+
+    def do_remove(self):
+        raise Exception("Unimplemented")
+
+    def keys(self):
+        raise Exception("Unimplemented")
+
+
+class DynomiteContainer(Container):
+    namespace_manager = DynomiteManager
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='beaker_extensions',
+      version=version,
+      description="Beaker extensions for additional back-end stores.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Didip Kerabat',
+      author_email='didpk@gmail.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
